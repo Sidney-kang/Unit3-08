2d76c643e3bb0093ccaf0c27a39b3529a1ccca0d
Sidney <sidney.kang@ocsbstudent.ca>
2017-10-28 14:05:19 -0400

    Initial version

---
+++ capital_letters.py
@@ -0,0 +1,14 @@
+# Created by : Sidney Kang
+# Created on : 23 Oct. 2017
+# Created for : ICS3UR
+# Daily Assignment - Unit3-08
+# This program displays a for loop program that prints capital letters
+#   from A - Z and lower case
+
+for code in range(65,91) :
+    #output          
+    print(str(unichr(code)) + " Code is: " + str(code))
+
+for code_2 in range(97,123):
+    #output          
+    print(str(unichr(code_2)) + " Code is: " + str(code_2))
